d8193b460278325938eb67d9d9d75acc73dcbcfc
snow <1215167396@qq.com>
2017-07-30 22:44:40 +0800

    First commit

---
+++ app/src/main/java/cn/sxh/snowfox/view/BottomButtonView.java
@@ -0,0 +1,19 @@
+package cn.sxh.snowfox.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RadioGroup;
+
+/**
+ * Created by snow on 2017/7/30.
+ */
+
+public class BottomButtonView extends RadioGroup {
+    public BottomButtonView(Context context) {
+        super(context);
+    }
+
+    public BottomButtonView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
