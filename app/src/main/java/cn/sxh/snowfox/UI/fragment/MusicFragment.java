0ea3f6a86f7ebff8360a57125ea64cbbfb04c94b
snowFox <1215167396@qq.com>
2017-08-22 19:48:15 +0800

    尝试修改策划菜单，将其改变为可以滑动的菜单栏

--- app/src/main/java/cn/sxh/snowfox/UI/fragment/MusicFragment.java
+++ app/src/main/java/cn/sxh/snowfox/UI/fragment/MusicFragment.java
@@ -1,16 +1,22 @@
 package cn.sxh.snowfox.UI.fragment;
 
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
 
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 import cn.sxh.snowfox.R;
 import cn.sxh.snowfox.base.NewBaseFragment;
 import cn.sxh.snowfox.view.fragmentView.MusicFragemntView;
@@ -19,7 +25,7 @@ import cn.sxh.snowfox.view.fragmentView.MusicFragemntView;
  * Created by snow on 2017/8/5.
  */
 
-public class MusicFragment extends NewBaseFragment implements MusicFragemntView{
+public class MusicFragment extends NewBaseFragment implements MusicFragemntView {
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.tabs)
@@ -32,6 +38,8 @@ public class MusicFragment extends NewBaseFragment implements MusicFragemntView{
     NavigationView navView;
     @BindView(R.id.drawer_layout)
     DrawerLayout drawerLayout;
+    @BindView(R.id.listView)
+    ListView listView;
 
 
     @Override
@@ -68,4 +76,5 @@ public class MusicFragment extends NewBaseFragment implements MusicFragemntView{
     public void initViewPager(List<String> listTab) {
 
     }
+
 }
