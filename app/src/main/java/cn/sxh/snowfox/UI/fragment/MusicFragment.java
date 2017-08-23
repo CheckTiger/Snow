32ab5f4cb7f84a69276101e710d20c7353d4dc97
snow <1215167396@qq.com>
2017-08-23 21:47:11 +0800

    修改musicFragment

--- app/src/main/java/cn/sxh/snowfox/UI/fragment/MusicFragment.java
+++ app/src/main/java/cn/sxh/snowfox/UI/fragment/MusicFragment.java
@@ -1,22 +1,17 @@
 package cn.sxh.snowfox.UI.fragment;
 
-import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ListView;
 
 import java.util.List;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.Unbinder;
 import cn.sxh.snowfox.R;
 import cn.sxh.snowfox.base.NewBaseFragment;
 import cn.sxh.snowfox.view.fragmentView.MusicFragemntView;
