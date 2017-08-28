b978105f32fcf204d8a82a381def7a480880c1e9
snowFox <1215167396@qq.com>
2017-08-28 19:39:54 +0800

    添加数据库greendao，修改snowfox类的美容，创建新闻表

--- green-dao/src/main/java/com/example/SnowFox.java
+++ green-dao/src/main/java/com/example/SnowFox.java
@@ -1,4 +1,43 @@
 package com.example;
 
+import org.greenrobot.greendao.generator.DaoGenerator;
+import org.greenrobot.greendao.generator.Entity;
+import org.greenrobot.greendao.generator.Schema;
+
 public class SnowFox {
+    public static void main(String[] args) throws Exception {
+        int version = 1;
+        String defaultJavaPackage = "cn.sxh.snow.greendao";
+        Schema schema = new Schema(version, defaultJavaPackage);
+        createTable(schema);
+        new DaoGenerator().generateAll(schema,"./app/src/main/java-gen");
+    }
+
+    private static void createTable(Schema schema) {
+        Entity entity = schema.addEntity("SnowFoxTable");
+        /**
+         * 频道名称
+         */
+        entity.addStringProperty("SnowFoxChannelName").notNull().primaryKey().index();
+        /**
+         * 频道id
+         */
+        entity.addStringProperty("SnowFoxChannelId").notNull();
+        /**
+         * 频道类型
+         */
+        entity.addStringProperty("SnowFoxChannelType").notNull();
+        /**
+         * 选中的频道
+         */
+        entity.addBooleanProperty("SnowFoxChannelSelect").notNull();
+        /**
+         * 频道的排序位置
+         */
+        entity.addIntProperty("SnowFoxChannelIndex").notNull();
+        /**
+         * 频道是否是固定的
+         */
+        entity.addBooleanProperty("SnowFoxChannelFixed");
+    }
 }
