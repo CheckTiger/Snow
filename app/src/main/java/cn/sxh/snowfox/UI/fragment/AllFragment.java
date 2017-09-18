7c86f83403c147ad73a52e90a3552b9069b1c169
snowFox <1215167396@qq.com>
2017-09-18 20:13:52 +0800

    数据库的初步学习

--- app/src/main/java/cn/sxh/snowfox/UI/fragment/AllFragment.java
+++ app/src/main/java/cn/sxh/snowfox/UI/fragment/AllFragment.java
@@ -4,9 +4,16 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.ListView;
 
+import com.socks.library.KLog;
+
+import org.greenrobot.greendao.query.Query;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import cn.sxh.greendao.TechnologyTable;
+import cn.sxh.greendao.TechnologyTableDao;
+import cn.sxh.snowfox.AppContext;
 import cn.sxh.snowfox.R;
 import cn.sxh.snowfox.UI.activity.MainActivity;
 import cn.sxh.snowfox.UI.activity.NewsActivity;
@@ -21,6 +28,7 @@ import cn.sxh.snowfox.base.BaseFragment;
  */
 
 public class AllFragment extends BaseFragment {
+    private static final String TAG = "AllFragment";
     private ListView mListView;
     private List<String> list = new ArrayList<>();
     private AllFragmentAdapter adapter;
@@ -37,6 +45,13 @@ public class AllFragment extends BaseFragment {
 
     @Override
     protected void initData() {
+        Query<TechnologyTable> technologyTableQuery = AppContext.getTechnologyTableDao().queryBuilder()
+                .where(TechnologyTableDao.Properties.SnowFoxChannelName.eq(true))
+                .orderAsc(TechnologyTableDao.Properties.SnowFoxChannelId).build();
+        List<TechnologyTable> channelListMine = technologyTableQuery.list();
+        for (int i = 0; i < channelListMine.size(); i++) {
+            KLog.e(TAG,channelListMine.get(i).getSnowFoxChannelName());
+        }
         for (int i = 0; i < 20; i++) {
             list.add("宋学虎" + i);
         }
