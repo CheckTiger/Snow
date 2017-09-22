0684f83e8f39b8115f8906447e89c3a5b63278a6
snow <1215167396@qq.com>
2017-09-23 00:17:23 +0800

    初始化自定义模块以及书写时钟控件

---
+++ app/src/main/java/cn/sxh/snowfox/UI/activity/ClockViewActivity.java
@@ -0,0 +1,15 @@
+package cn.sxh.snowfox.UI.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.sxh.snowfox.R;
+
+public class ClockViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_clock_view);
+    }
+}
--- app/src/main/java/cn/sxh/snowfox/UI/fragment/DefinedFragment.java
+++ app/src/main/java/cn/sxh/snowfox/UI/fragment/DefinedFragment.java
@@ -1,12 +1,19 @@
 package cn.sxh.snowfox.UI.fragment;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
+import android.content.Intent;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import cn.sxh.snowfox.AppContext;
+import cn.sxh.snowfox.R;
+import cn.sxh.snowfox.UI.activity.ClockViewActivity;
+import cn.sxh.snowfox.UI.activity.NewsActivity;
+import cn.sxh.snowfox.adapter.AllFragmentAdapter;
+import cn.sxh.snowfox.base.BaseFragment;
 
 /**
  * @author by snow on 2017/9/9
@@ -14,13 +21,36 @@ import android.widget.TextView;
  * @mail snowtigersong@gmail.com
  */
 
-public class DefinedFragment extends Fragment {
-    @Nullable
+public class DefinedFragment extends BaseFragment {
+    private static final String TAG = "AllFragment";
+    private ListView mListView;
+    private List<String> list = new ArrayList<>();
+    private AllFragmentAdapter adapter;
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        TextView textView = new TextView(getContext());
-        textView.setText("自定义");
-        return textView;
+    protected int getContentView() {
+        return R.layout.all_fragment_layout;
     }
 
+    @Override
+    protected void initUI(View view) {
+        mListView = view.findViewById(R.id.all_fragment_listView);
+        adapter = new AllFragmentAdapter(getContext(),list);
+    }
+
+    @Override
+    protected void initData() {
+        List<String> technologyName = Arrays.asList(AppContext.getInstance().getResources().getStringArray(R.array.all_defined_view));
+        for (int i = 0; i < technologyName.size(); i++) {
+            list.add(technologyName.get(i));
+        }
+        mListView.setAdapter(adapter);
+        adapter.setOnLinearLayoutListener((holder, position) -> gotoActivity( position));
+    }
+    private void gotoActivity(int position) {
+        switch (position) {
+            case 0:
+                Intent intent = new Intent(getActivity(), ClockViewActivity.class);
+                startActivity(intent);
+        }
+    }
 }
