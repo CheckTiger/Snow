604ba7b2a46e97a451d5362119a85bdcb46e8029
snowFox <1215167396@qq.com>
2017-10-31 19:51:23 +0800

    画笔知识学习

--- app/src/main/java/cn/sxh/snowfox/UI/activity/PaintActivity.java
+++ app/src/main/java/cn/sxh/snowfox/UI/activity/PaintActivity.java
@@ -13,26 +13,12 @@ import cn.sxh.snowfox.R;
 
 public class PaintActivity extends AppCompatActivity {
 
-    @BindView(R.id.tabLayout)
-    TabLayout tabLayout;
-    @BindView(R.id.pager)
-    ViewPager pager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paint);
-        ButterKnife.bind(this);
     }
-    private class PageModel {
-        @LayoutRes int sampleLayoutRes;
-        @StringRes int titleRes;
-        @LayoutRes int practiceLayoutRes;
 
-        PageModel(@LayoutRes int sampleLayoutRes, @StringRes int titleRes, @LayoutRes int practiceLayoutRes) {
-            this.sampleLayoutRes = sampleLayoutRes;
-            this.titleRes = titleRes;
-            this.practiceLayoutRes = practiceLayoutRes;
-        }
-    }
 }
---
+++ app/src/main/java/cn/sxh/snowfox/view/DefinedAndCollectView/PaintKnowledgeView.java
@@ -0,0 +1,64 @@
+package cn.sxh.snowfox.view.DefinedAndCollectView;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Shader;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2017/10/31 0031.
+ */
+
+public class PaintKnowledgeView extends View {
+    /**
+     * Shader.TileMode 是枚举值，有三个值CLAMP 、REPEAT 和 MIRROR
+     * 1.CLAMP表示。当所画图形的尺寸大于给定的区域，会用给定区域四边的颜色来填充剩余的空间
+     * 2.REPEAT表示，当我们绘制的图形尺寸大于Bitmap尺寸时，会用Bitmap重复平铺整个绘制的区域。
+     * 3.MIRROR表示，当绘制的图形尺寸大于Bitmap尺寸时，MIRROR也会用Bitmap重复平铺整个绘图区域，
+     * 与REPEAT不同的是，两个相邻的Bitmap互为镜像。
+     */
+    public int DEFAULT_TYPE = 1;//线性渐变
+    public static final int LINEAR_GRADIENT = 1;//线性渐变
+    public static final int RadialGradient  = 1;//线性渐变
+    private Paint mPaint;//画笔
+    private Shader mShader;//着色器
+    public PaintKnowledgeView(Context context) {
+        this(context,null);
+    }
+
+    public PaintKnowledgeView(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public PaintKnowledgeView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        if (DEFAULT_TYPE == LINEAR_GRADIENT) {
+            mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            mShader = new LinearGradient(100, 100, 500, 500,
+                    Color.parseColor("#E91E63"), Color.parseColor("#2196F3"), Shader.TileMode.CLAMP);
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        if (DEFAULT_TYPE == LINEAR_GRADIENT) {
+            mPaint.setShader(mShader);
+            canvas.drawCircle(300,300,200,mPaint);
+        }
+
+    }
+
+    public void setViewType(int type) {
+        this.DEFAULT_TYPE = type;
+    }
+}
